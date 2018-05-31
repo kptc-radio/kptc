5efa2d618e46e5cc7e022d09925822c773904797
Sebastian Martin Dicke <Sebastianmartindicke@gmx.de>
2018-05-31 21:33:01 +0200

    Scoped an enumeration

--- src/configdata.cpp
+++ src/configdata.cpp
@@ -178,146 +178,146 @@ QString ConfigData::getPort() {
 }
 
 void ConfigData::setEditPort(const QString &dev) {
-	this->setData(PORTDEVICE_EDIT, dev);
+	this->setData(Types::PORTDEVICE_EDIT, dev);
 //	this->setValue(Group::PORT, "PORTDEVICE_EDIT", dev);
 }
 
 QString ConfigData::getEditPort() {
-	return this->getData(PORTDEVICE_EDIT);
+	return this->getData(Types::PORTDEVICE_EDIT);
 //	return this->getValue(Group::PORT, "PORTDEVICE_EDIT", "/dev/ttyS11");
 }
 
 void ConfigData::setPortSpeed(const QString &speed) {
 	if (speed != 0L) {
-		this->setData(PORTSPEED, speed);
+		this->setData(Types::PORTSPEED, speed);
 		//this->setValue(Group::PORT, "PORTSPEED", speed);
 	}
 }
 
 QString ConfigData::getPortSpeed() {
-	return this->getData(PORTSPEED);
+	return this->getData(Types::PORTSPEED);
 //	return this->getValue(Group::PORT, "PORTSPEED", "38400");
 }
 
 void ConfigData::setCall(const QString &call) {
-	this->setData(CALL, call);
+	this->setData(Types::CALL, call);
 //	this->setValue(Group::PERSONAL, "CALL", call);
 }
 
 
 QString ConfigData::getCall() {
-	return this->getData(CALL);
+	return this->getData(Types::CALL);
 //	return this->getValue(Group::PERSONAL, "CALL", "DK0TUX");
 }
 
 void ConfigData::setSelCall(const QString &call) {
-	this->setData(SELCALL, call);
+	this->setData(Types::SELCALL, call);
 //	this->setValue(Group::PERSONAL, "SELCALL", call);
 }
 
 
 QString ConfigData::getSelCall() {
-	return this->getData(SELCALL);
+	return this->getData(Types::SELCALL);
 //	return this->getValue(Group::PERSONAL, "SELCALL", "DTUX");
 }
 
 
 void ConfigData::setfirststart(bool  first) {
 	if (!first) {
-		this->setData(FIRSTSTART, "FALSE");
+		this->setData(Types::FIRSTSTART, "FALSE");
 	}
 //	if (!first) this->setValue(Group::GENERAL, "FIRSTSTART", "FALSE");
 }
 
 bool ConfigData::firststart() {
-	QString s = this->getData(FIRSTSTART);
+	QString s = this->getData(Types::FIRSTSTART);
 //	QString s=  this->getValue(Group::GENERAL, "FIRSTSTART", "TRUE");
 	//qDebug () << "configdata::firststart() - entry:" << s;
 	return this->stringIsTrue(s);
 }
 
 void ConfigData::setAwayMsg(bool set,const QString &msg) {
-	this->setData(USEAWAYMSG, this->boolToString(set));
-	this->setData(AWAYMSG, msg);
+	this->setData(Types::USEAWAYMSG, this->boolToString(set));
+	this->setData(Types::AWAYMSG, msg);
 //	this->setValue(Group::LOGOUT, "USEAWAYMSG", this->boolToString(set));
 //	this->setValue(Group::LOGOUT, "AWAYMSG", msg);
 }
 
 bool ConfigData::isAwayMsg() {
-	QString string = this->getData(USEAWAYMSG);
+	QString string = this->getData(Types::USEAWAYMSG);
 //	QString s= this->getValue(Group::LOGOUT, "USEAWAYMSG", "TRUE");
 	return this->stringIsTrue(string);
 }
 
 QString ConfigData::getAwayMsg() {
-	return this->getData(AWAYMSG);
+	return this->getData(Types::AWAYMSG);
 //	return this->getValue(Group::LOGOUT, "AWAYMSG", "Here is %MYCALL% from %MYQTH%,\n\ncurrently terminal is not active!\n\n73 de %MYNAME%.");
 }
 /////
 void ConfigData::setCMsg(bool set,const QString &msg) {
-	this->setData(USECMSG, this->boolToString(set));
-	this->setData(CMSG, msg);
+	this->setData(Types::USECMSG, this->boolToString(set));
+	this->setData(Types::CMSG, msg);
 //	this->setValue(Group::PERSONAL, "USECMSG", this->boolToString(set));
 //	this->setValue(Group::PERSONAL, "CMSG", msg);
 }
 
 bool ConfigData::isCMsg() {
-	QString string = this->getData(USECMSG);
+	QString string = this->getData(Types::USECMSG);
 //	QString s= this->getValue(Group::PERSONAL, "USECMSG", "TRUE");
 	return this->stringIsTrue(string);
 }
 
 QString ConfigData::getCMsg() {
-	return this->getData(CMSG);
+	return this->getData(Types::CMSG);
 //	return this->getValue(Group::PERSONAL, "CMSG", "Here is %MYCALL% from %MYQTH%!");
 }
 
 void ConfigData::setCQPactor(const QString &qtext) {
-	this->setData(PACTOR, qtext);
+	this->setData(Types::PACTOR, qtext);
 //	this->setValue(Group::CQTEXT, "PACTOR", qtext);
 }
 
 QString ConfigData::getCQPactor() {
-	return this->getData(PACTOR);
+	return this->getData(Types::PACTOR);
 //	return this->getValue(Group::CQTEXT, "PACTOR", "CQ CQ CQ de %MYCALL%\nCQ CQ CQ de %MYCALL%\nCQ CQ CQ de %MYCALL%\npse K\n");
 }
 
 void ConfigData::setCQAmtor(const QString &qtext) {
-	this->setData(AMTOR, qtext);
+	this->setData(Types::AMTOR, qtext);
 //	this->setValue(Group::CQTEXT, "AMTOR", qtext);
 }
 
 QString ConfigData::getCQAmtor() {
-	return this->getData(AMTOR);
+	return this->getData(Types::AMTOR);
 //	return this->getValue(Group::CQTEXT, "AMTOR", "CQ CQ CQ de %MYCALL%\nCQ CQ CQ de %MYCALL%\nCQ CQ CQ de %MYCALL%\npse K\n");
 }
 
 void ConfigData::setCQRTTY(const QString &qtext) {
-	this->setData(RTTY, qtext);
+	this->setData(Types::RTTY, qtext);
 //	this->setValue(Group::CQTEXT, "RTTY", qtext);
 }
 
 QString ConfigData::getCQRTTY() {
-	return this->getData(RTTY);
+	return this->getData(Types::RTTY);
 //	return this->getValue(Group::CQTEXT, "RTTY", "ryryryryryry\nCQ CQ CQ de %MYSELCALL%\nCQ CQ CQ de %MYSELCALL%\npse K\n");
 }
 void ConfigData::setCQPSK31(const QString &qtext) {
-	this->setData(PSK31, qtext);
+	this->setData(Types::PSK31, qtext);
 //	this->setValue(Group::CQTEXT, "PSK31", qtext);
 }
 
 QString ConfigData::getCQPSK31() {
-	return this->getData(PSK31);
+	return this->getData(Types::PSK31);
 //	return this->getValue(Group::CQTEXT, "PSK31", "CQ CQ CQ de %MYCALL%\nCQ CQ CQ de %MYCALL%\nCQ CQ CQ de %MYCALL%\npse K\n");
 }
 
 void ConfigData::setCQCW(const QString &qtext) {
-	this->setData(CW, qtext);
+	this->setData(Types::CW, qtext);
 //	this->setValue(Group::CQTEXT, "CW", qtext);
 }
 
 QString ConfigData::getCQCW() {
-	return this->getData(CW);
+	return this->getData(Types::CW);
 //	return this->getValue(Group::CQTEXT, "CW", "cq cq cq de %MYCALL%\ncq cq cq de %MYCALL%\npse k\n");
 }
 
@@ -339,64 +339,64 @@ QString ConfigData::getFixPath (const QString &number) {
 }
 
 void ConfigData::setName(const QString &name) {
-	this->setData(NAME, name);
+	this->setData(Types::NAME, name);
 //	this->setValue(Group::PERSONAL, "NAME", name);
 }
 
 QString ConfigData::getName() {
-	return this->getData(NAME);
+	return this->getData(Types::NAME);
 //	return this->getValue(Group::PERSONAL, "NAME", "Tux");
 }
 
 void ConfigData::setQTH(const QString &name) {
-	this->setData(QTH, name);
+	this->setData(Types::QTH, name);
 //	this->setValue(Group::PERSONAL, "QTH", name);
 }
 
 QString ConfigData::getQTH() {
-	return this->getData(QTH);
+	return this->getData(Types::QTH);
 //	return this->getValue(Group::PERSONAL, "QTH", "Antarctic");
 }
 
 void ConfigData::setLogoutPath(const QString &path) {
-	this->setData(PATH_LOGOUT, path);
+	this->setData(Types::PATH_LOGOUT, path);
 //	this->setValue(Group::LOGOUT, "PATH", path);
 }
 
 QString ConfigData::getLogoutPath () {
-	return this->getData(PATH_LOGOUT);
+	return this->getData(Types::PATH_LOGOUT);
 //	return this->getValue(Group::LOGOUT, "PATH", "");
 }
 
 
 void ConfigData::setLoginPath(const QString &path) {
-	this->setData(PATH_LOGIN, path);
+	this->setData(Types::PATH_LOGIN, path);
 //	this->setValue(Group::LOGIN, "PATH", path);
 }
 
 QString ConfigData::getLoginPath () {
-	return this->getData(PATH_LOGIN);
+	return this->getData(Types::PATH_LOGIN);
 //	return this->getValue(Group::LOGIN, "PATH", "");
 }
 
 void ConfigData::setLogoutScript(bool set) {
 	QString qset = this->boolToString(set);
-	this->setData(LOGOUTSCRIPT, qset);
+	this->setData(Types::LOGOUTSCRIPT, qset);
 //	this->setValue(Group::LOGOUT, "LOGOUTSCRIPT", qset);
 }
 
 bool ConfigData::isLogoutScript() {
-	QString temp = this->getData(LOGOUTSCRIPT);
+	QString temp = this->getData(Types::LOGOUTSCRIPT);
 	return this->stringIsTrue(temp);
 }
 
 void ConfigData::setLoginScript(bool set) {
 	QString qset = this->boolToString(set);
-	this->setData(LOGINSCRIPT, qset);
+	this->setData(Types::LOGINSCRIPT, qset);
 //	this->setValue(Group::LOGIN, "LOGINSCRIPT", qset);
 }
 
 bool ConfigData::isLoginScript() {
-	QString temp = this->getData(LOGINSCRIPT);
+	QString temp = this->getData(Types::LOGINSCRIPT);
 	return this->stringIsTrue(temp);
 }
